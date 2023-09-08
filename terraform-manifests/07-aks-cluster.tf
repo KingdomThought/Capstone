c46852604dc2d041837ba740a6346aecbec47121
Delano Dinnall <79799753+KingdomThought@users.noreply.github.com>
2023-09-08 16:23:21 -0400

    Update 07-aks-cluster.tf-cjanges to 07--aks-cluster-tf

--- terraform-manifests/07-aks-cluster.tf
+++ terraform-manifests/07-aks-cluster.tf
@@ -7,16 +7,16 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   node_resource_group = "${azurerm_resource_group.aks_rg.name}-nrg"
 
   default_node_pool {
-    name                = "systempool"
-    vm_size             = "Standard_DS2_v2"
-    orchestrator_version= data.azurerm_kubernetes_service_versions.current.latest_version
-    availability_zones  = [1, 2, 3]
-    enable_auto_scaling = true
-    max_count           = 1
-    min_count           = 1
-    os_disk_size_gb     = 30
-    type                = "VirtualMachineScaleSets"
-    
+    name                 = "systempool"
+    vm_size              = "Standard_DS2_v2"
+    orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
+    availability_zones   = [1, 2, 3]
+    enable_auto_scaling  = true
+    max_count            = 1
+    min_count            = 1
+    os_disk_size_gb      = 30
+    type                 = "VirtualMachineScaleSets"
+
     node_labels = {
       "nodepool-type" = "system"
       "environment"   = var.environment
@@ -31,12 +31,10 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     }    
   }
 
-  # Identity (System Assigned or Service Principal)
   identity { 
     type = "SystemAssigned" 
   }
 
-  # Add On Profiles
   addon_profile {
     azure_policy { 
       enabled = true 
@@ -47,22 +45,18 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     }
   }
 
-  # RBAC and Azure AD Integration Block
-  role_based_access_control {
-    enabled = true
-    azure_active_directory {
-      managed                = true
-      admin_group_object_ids = [azuread_group.aks_administrators.id]
-    }
+  role_based_access_control_enabled = true
+
+  azure_active_directory_role_based_access_control {
+    managed                = true
+    admin_group_object_ids = [azuread_group.aks_administrators.id]
   }
 
-  # Windows Admin Profile
   windows_profile {
     admin_username = var.windows_admin_username
     admin_password = var.windows_admin_password
   }
 
-  # Linux Profile
   linux_profile {
     admin_username = "ubuntu"
     ssh_key {
@@ -70,13 +64,11 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     }
   }
 
-  # Network Profile
   network_profile {
     load_balancer_sku = "Standard"
     network_plugin    = "azure"
   }
 
-  # AKS Cluster Tags 
   tags = {
     Environment = var.environment
   }
